676971982ab6e8ef11bea6eeefe16e15231e0f05
wasim <wasim@192.168.192.1>
2023-04-15 13:49:09 +0530

    update

---
+++ src/test/java/genral/Parallel.java
@@ -0,0 +1,22 @@
+package genral;
+
+import org.testng.annotations.Test;
+
+public class Parallel {
+	@Test
+	public void demo()
+	{
+		System.out.println("hii");
+	}
+	
+	public void demo2()
+	{
+		System.out.println("hello");
+	}
+	
+	public void demo3()
+	{
+		System.out.println("wasim bhullakad");
+	}
+
+}
